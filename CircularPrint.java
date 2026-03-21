48a50738f2e06b2c39cc98fc23774cc518e928b2
Sai deekshith <orugantideekshith2006@gmail.com>
2026-03-21 16:17:30 +0530

    Create CircularPrint.java

---
+++ CircularPrint.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        int k=sc.nextInt(),arr[]=new int[n];;
+
+        for(int i=0;i<n;i++)
+         arr[i]=sc.nextInt();
+
+        for(int i=k;i<n;i++) System.out.print(arr[i]+ " "); 
+        for(int i=0;i<k;i++)System.out.print(arr[i]+ " ");
+    }
+}
