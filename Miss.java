54378e0619b96daa368a66e7f02f4cacd52e85ab
Sai deekshith <orugantideekshith2006@gmail.com>
2026-04-14 22:58:20 +0530

    Remove placeholder comment in Miss.java
    Removed placeholder comment for code entry.

--- Miss.java
+++ Miss.java
@@ -3,7 +3,6 @@ import java.util.*;
 public class Miss {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Main. */
         int sum=0;
         Scanner s=new Scanner(System.in);
         for(int i=0;i<99;i++) {
