4eff40de3becfdcd26c56b36cb53a86ce5f92073
Sai deekshith <orugantideekshith2006@gmail.com>
2026-06-22 23:40:30 +0530

    Implement spiral matrix generation algorithm

--- Spiral Matrix II.java
+++ Spiral Matrix II.java
@@ -1,1 +1,42 @@
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int[][] matrix = new int[n][n];
 
+        int top = 0, bottom = n - 1;
+        int left = 0, right = n - 1;
+        int num = 1;
+
+        while (top <= bottom && left <= right) {
+
+            // Left to Right
+            for (int j = left; j <= right; j++) {
+                matrix[top][j] = num++;
+            }
+            top++;
+
+            // Top to Bottom
+            for (int i = top; i <= bottom; i++) {
+                matrix[i][right] = num++;
+            }
+            right--;
+
+            // Right to Left
+            if (top <= bottom) {
+                for (int j = right; j >= left; j--) {
+                    matrix[bottom][j] = num++;
+                }
+                bottom--;
+            }
+
+            // Bottom to Top
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    matrix[i][left] = num++;
+                }
+                left++;
+            }
+        }
+
+        return matrix;
+    }
+}
