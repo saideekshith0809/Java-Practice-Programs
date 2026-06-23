2081c04146c1c5bc986f9595a2443fa59fc69f56
Sai deekshith <orugantideekshith2006@gmail.com>
2026-06-23 23:22:33 +0530

    Update JumpGameII.java

--- JumpGameII.java
+++ JumpGameII.java
@@ -6,7 +6,6 @@ class Solution {
         int jumps = 0;
         int currentEnd = 0;
         int farthest = 0;
-
         for (int i = 0; i < nums.length - 1; i++) {
             farthest = Math.max(farthest, i + nums[i]);
 
