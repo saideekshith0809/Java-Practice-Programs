c53eca60b057cb5471e3f09c9f7032cf1451273a
Sai deekshith <orugantideekshith2006@gmail.com>
2026-07-10 22:12:02 +0530

    Update 2D-ARRAY.java

--- 2D-ARRAY.java
+++ 2D-ARRAY.java
@@ -1,1 +1,36 @@
+import java.util.*;
 
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[][] arr = new int[6][6];
+
+        // Input
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        int maxSum = Integer.MIN_VALUE;
+
+        // Check all possible hourglasses
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+
+                int sum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2]
+                        + arr[i + 1][j + 1]
+                        + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+
+                if (sum > maxSum) {
+                    maxSum = sum;
+                }
+            }
+        }
+
+        System.out.println(maxSum);
+        sc.close();
+    }
+}
