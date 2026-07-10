9c0da88dce5f03cb0c4980d42797f0ba6cd6356e
Sai deekshith <orugantideekshith2006@gmail.com>
2026-07-10 22:12:10 +0530

    Update 2D-ARRAY.java

--- 2D-ARRAY.java
+++ 2D-ARRAY.java
@@ -7,7 +7,6 @@ public class Solution {
 
         int[][] arr = new int[6][6];
 
-        // Input
         for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 6; j++) {
                 arr[i][j] = sc.nextInt();
