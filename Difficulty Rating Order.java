d4597754194d3c0026fae07a6d6ce0d0e3ec8a1f
Sai deekshith <orugantideekshith2006@gmail.com>
2026-07-14 11:45:16 +0530

    Update Difficulty Rating Order.java

--- Difficulty Rating Order.java
+++ Difficulty Rating Order.java
@@ -1,1 +1,33 @@
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int t = scanner.nextInt();
+
+        while (t-- > 0) {
+            int n = scanner.nextInt();
+            int[] d = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                d[i] = scanner.nextInt();
+            }
+
+            boolean ok = true;
+
+            for (int i = 1; i < n; i++) {
+                if (d[i] < d[i - 1]) {
+                    ok = false;
+                    break;
+                }
+            }
+
+            if (ok) {
+                System.out.println("Yes");
+            } else {
+                System.out.println("No");
+            }
+        }
+    }
+}
