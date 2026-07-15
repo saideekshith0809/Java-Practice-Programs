cc21e21853bfc00d9263eaba11061ff27457115d
Sai deekshith <orugantideekshith2006@gmail.com>
2026-07-15 20:49:22 +0530

    Update Square Paper.java

--- Square Paper.java
+++ Square Paper.java
@@ -1,1 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc = new Scanner(System.in);
+	    int A=sc.nextInt();
+	    int B=sc.nextInt();
+	    int area=1;
+	    if(A>B){
+	         area=B;
+	    }
+	    else{
+	        area=A;
+	    }
+	    System.out.println(area*area);
+
+	}
+}
 
