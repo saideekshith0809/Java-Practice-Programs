34585da5ebb6db1fec36fd0dfc043cba2e8fe0e9
Sai deekshith <orugantideekshith2006@gmail.com>
2026-07-20 18:09:30 +0530

    Update Binary Gap.java

--- Binary Gap.java
+++ Binary Gap.java
@@ -1,1 +1,17 @@
+class Solution {
+    public int binaryGap(int n) {
+        String st=Integer.toBinaryString(n);
+        int prev=-1;
+        int ans=0;
 
+        for (int i=0;i<st.length();i++) {
+            if (st.charAt(i) == '1') {
+                if (prev!=-1) {
+                    ans=Math.max(ans,i-prev);
+                }
+                prev=i;
+            }
+        }
+        return ans;
+    }
+}
