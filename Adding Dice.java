1140c60d057c9ddad732e52134d7a839ac9b43c9
Sai deekshith <orugantideekshith2006@gmail.com>
2026-07-22 23:09:47 +0530

    Add Dice.java for input validation

--- Adding Dice.java
+++ Adding Dice.java
@@ -1,1 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc = new Scanner(System.in);
+	    int n = sc.nextInt();
+	    int y = 9-n;
+	    if(y>=1 && y<=6){
+	        System.out.println("Yes");
+	    }
+	    else{
+	        System.out.println("No");
+	    }
+	    
+
+
+	}
+}
 
