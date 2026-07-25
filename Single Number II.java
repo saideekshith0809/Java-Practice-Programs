9ed5e2a97d3f67aa1b3b38b27fe26e23be9555fa
Sai deekshith <orugantideekshith2006@gmail.com>
2026-07-25 22:07:59 +0530

    Update Single Number II.java

--- Single Number II.java
+++ Single Number II.java
@@ -1,1 +1,19 @@
+class Solution {
+    public int singleNumber(int[] a) {
+        int n = a.length;
 
+        for (int i = 0; i < n; i++) {
+            int count = 1;
+
+            for (int j = 0; j < n; j++) {
+                if (i != j && a[i] == a[j])
+                    count++;
+            }
+
+            if (count == 1)
+                return a[i];
+        }
+
+        return -1;
+    }
+}
