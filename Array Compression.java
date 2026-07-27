aebc952b1e98b8b4e024df9239a2a8f96a050af6
Sai deekshith <orugantideekshith2006@gmail.com>
2026-07-27 23:53:23 +0530

    Update Array Compression.java

--- Array Compression.java
+++ Array Compression.java
@@ -1,1 +1,24 @@
+import java.util.*;
 
+class Codechef {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] a = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        int count = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (a[i] != a[i - 1]) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
