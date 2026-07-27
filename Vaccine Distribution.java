dcacf2a296191a9236699318d1119a1f8fddad08
Sai deekshith <orugantideekshith2006@gmail.com>
2026-07-27 23:52:20 +0530

    Update Vaccine Distribution.java

--- Vaccine Distribution.java
+++ Vaccine Distribution.java
@@ -1,1 +1,32 @@
+import java.util.*;
 
+class Codechef {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] v = new int[n];
+        int[] p = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            v[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            p[i] = sc.nextInt();
+        }
+
+        Arrays.sort(v);
+        Arrays.sort(p);
+
+        for (int i = 0; i < n; i++) {
+            if (v[i] <= p[i]) {
+                System.out.println("No");
+                return;
+            }
+        }
+
+        System.out.println("Yes");
+    }
+}
