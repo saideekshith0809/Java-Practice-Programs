56e2fe5c179fff8406ae027996daa22e9a639694
Sai deekshith <orugantideekshith2006@gmail.com>
2026-07-29 22:36:55 +0530

    Implement digit comparison in Varied Digits.java
    Add logic to check if the first and last digits of an integer are the same.

--- Varied Digits.java
+++ Varied Digits.java
@@ -1,1 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		    int first=x%10;
+		    int last=x/10;
+		if(first==last){
+		    System.out.println("No");
+		}
+		else{
+		    System.out.println("Yes");
+		}
+
+	}
+}
