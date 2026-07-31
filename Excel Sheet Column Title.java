2cb100573b8a1a75df6a48e49d1c46d9d927f81c
Sai deekshith <orugantideekshith2006@gmail.com>
2026-07-31 23:02:07 +0530

    Update Excel Sheet Column Title.java

--- Excel Sheet Column Title.java
+++ Excel Sheet Column Title.java
@@ -1,1 +1,14 @@
+class Solution {
+    public String convertToTitle(int columnNumber) {
+        StringBuilder ans = new StringBuilder();
 
+        while (columnNumber > 0) {
+            columnNumber--; // Convert to 0-based
+            int rem = columnNumber % 26;
+            ans.append((char) ('A' + rem));
+            columnNumber /= 26;
+        }
+
+        return ans.reverse().toString();
+    }
+}
