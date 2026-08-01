9e7707014b4cbaecb5e62626d1a4c52c88ba870c
Sai deekshith <orugantideekshith2006@gmail.com>
2026-08-01 22:07:43 +0530

    Update Maximum Row Sum.java

--- Maximum Row Sum.java
+++ Maximum Row Sum.java
@@ -1,1 +1,28 @@
+import java.util.*;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        long maxSum = Long.MIN_VALUE;
+        int maxRow = 0;
+
+        for (int i = 0; i < N; i++) {
+            long sum = 0;
+
+            for (int j = 0; j < M; j++) {
+                sum += sc.nextInt();
+            }
+
+            if (sum > maxSum) {
+                maxSum = sum;
+                maxRow = i;
+            }
+        }
+
+        System.out.println(maxRow);
+    }
+}
