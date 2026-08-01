f434cbed7bd65276ea2539e286058c8a8ef725bf
Sai deekshith <orugantideekshith2006@gmail.com>
2026-08-01 22:06:56 +0530

    Update H Pattern.java

--- H Pattern.java
+++ H Pattern.java
@@ -1,1 +1,28 @@
+import java.io.*;
+import java.util.*;
 
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int a =1;a<=t;a++){
+            System.out.println("Case #"+a+":");
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            for(int i=0;i<n;i++){
+                for(int j=0;j<m;j++){
+                    if(i==(n-1)/2 || j==0 || j==m-1){
+                        System.out.print("*");
+                    }
+                    else{
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println();
+            }
+
+        }
+
+    }
+}
