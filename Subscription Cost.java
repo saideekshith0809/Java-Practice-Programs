1c4cceeac219e448481d43765c70e83cc363bc44
Sai deekshith <orugantideekshith2006@gmail.com>
2026-08-07 21:39:11 +0530

    Update Subscription Cost.java

--- Subscription Cost.java
+++ Subscription Cost.java
@@ -1,1 +1,20 @@
+import java.util.*;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+            int N = sc.nextInt();
+            int X = sc.nextInt();
+            int Y = sc.nextInt();
+
+            if (N <= 3)
+                System.out.println(N * X);
+            else
+                System.out.println(3 * X + (N - 3) * Y);
+        }
+    }
+}
